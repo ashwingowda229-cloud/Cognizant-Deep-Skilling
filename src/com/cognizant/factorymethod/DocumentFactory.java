d7c91f4f0ac5940d54a3afa1aec7d119ca55f65e
Ashwin Gowda <yourgithubemail@gmail.com>
2026-07-02 19:29:12 +0530

    Added Factory Method Pattern

---
+++ src/com/cognizant/factorymethod/DocumentFactory.java
@@ -0,0 +1,17 @@
+package com.cognizant.factorymethod;
+
+public class DocumentFactory {
+
+    public static Document createDocument(String type) {
+
+        if (type.equalsIgnoreCase("WORD")) {
+            return new WordDocument();
+        } else if (type.equalsIgnoreCase("PDF")) {
+            return new PdfDocument();
+        } else if (type.equalsIgnoreCase("EXCEL")) {
+            return new ExcelDocument();
+        }
+
+        return null;
+    }
+}
