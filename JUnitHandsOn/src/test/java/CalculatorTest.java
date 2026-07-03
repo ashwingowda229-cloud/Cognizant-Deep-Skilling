0989133f069f33bccec04e035c3bc8ad1b3f69e7
Ashwin Gowda <yourgithubemail@gmail.com>
2026-07-03 15:08:24 +0530

    Add JUnit, Mockito and SLF4J hands-on exercises

---
+++ JUnitHandsOn/src/test/java/CalculatorTest.java
@@ -0,0 +1,33 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAdd() {
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+    }
+}
