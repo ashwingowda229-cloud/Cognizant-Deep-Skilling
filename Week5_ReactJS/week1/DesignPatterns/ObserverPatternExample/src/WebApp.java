29450e0a0c4bc219ca1a69700c51f887f86d876e
Ashwin Gowda <yourgithubemail@gmail.com>
2026-07-20 00:12:27 +0530

    Organized Week5 ReactJS projects

--- Week5_ReactJS/week1/DesignPatterns/ObserverPatternExample/src/WebApp.java
+++
@@ -1,7 +0,0 @@
-public class WebApp implements Observer {
-
-    @Override
-    public void update(double stockPrice) {
-        System.out.println("Web App: Stock price updated to " + stockPrice);
-    }
-}
--- studentapp/src/Components/Contact.js
+++
@@ -1,7 +0,0 @@
-function Contact() {
-    return (
-        <h2>Welcome to the Contact page of the Student Management Portal</h2>
-    );
-}
-
-export default Contact;
